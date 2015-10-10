14ca0b6aecd7ceeb87aa8b5ff5f7a326c516a6ec
Dmitry Ovchinnikov <ovchinnikov.d94@gmail.com>
2015-10-10 08:41:52 +0300

    first commit

---
+++ InsultDetector.py
@@ -0,0 +1,15 @@
+
+
+__author__ = 'Dimtriy Ovchinnikov'
+
+class InsultDetector:
+	
+	def __init__(self):
+		None
+
+	def train(self, discussions):
+		None
+
+	def getClasses(self, discussions):
+		None
+
